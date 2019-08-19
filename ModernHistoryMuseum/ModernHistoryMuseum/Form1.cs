6e5eed7739212d8c13f850671aa3b871eeed8b15
LOUW PRETORIUS <30373646@student365.msfed.nwu.ac.za>
2019-08-19 10:44:09 +0200

    Button added

--- ModernHistoryMuseum/ModernHistoryMuseum/Form1.cs
+++ ModernHistoryMuseum/ModernHistoryMuseum/Form1.cs
@@ -16,5 +16,10 @@ namespace ModernHistoryMuseum
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Robbie is big gay");
+        }
     }
 }
