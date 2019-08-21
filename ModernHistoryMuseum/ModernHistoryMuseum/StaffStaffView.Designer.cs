a7334c84868a6c0ba714c5059e2f424cc9d61872
Tokoshima <54238068+Tokoshima@users.noreply.github.com>
2019-08-21 18:13:20 +0200

    Updated Staff Staff view

--- ModernHistoryMuseum/ModernHistoryMuseum/StaffStaffView.Designer.cs
+++ ModernHistoryMuseum/ModernHistoryMuseum/StaffStaffView.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvStaffStaffView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaffStaffView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStaffStaffView
+            // 
+            this.dgvStaffStaffView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStaffStaffView.Location = new System.Drawing.Point(12, 54);
+            this.dgvStaffStaffView.Name = "dgvStaffStaffView";
+            this.dgvStaffStaffView.RowTemplate.Height = 24;
+            this.dgvStaffStaffView.Size = new System.Drawing.Size(776, 384);
+            this.dgvStaffStaffView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Staff View";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(656, 450);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(132, 23);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // StaffStaffView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvStaffStaffView);
+            this.Name = "StaffStaffView";
             this.Text = "StaffStaff";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaffStaffView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvStaffStaffView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBack;
     }
 }
