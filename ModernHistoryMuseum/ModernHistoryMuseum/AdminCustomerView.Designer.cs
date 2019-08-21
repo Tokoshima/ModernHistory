833ca5472a4a9fd13b065fabf6c04c4a4cdde6f7
ROBBIE VAN BREDA <25046659@student365.msfed.nwu.ac.za>
2019-08-21 18:21:30 +0200

    Changes on Admin View

--- ModernHistoryMuseum/ModernHistoryMuseum/AdminCustomerView.Designer.cs
+++ ModernHistoryMuseum/ModernHistoryMuseum/AdminCustomerView.Designer.cs
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(894, 485);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.dgvAdminCustomer);
             this.Name = "AdminCustomerView";
